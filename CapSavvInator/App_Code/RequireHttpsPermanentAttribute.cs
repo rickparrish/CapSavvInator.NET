278ee66c67e400db16cc5bcfcf9387df3aa6d03d
rickparrish <github@rick.parrish>
2015-06-08 11:15:06 -0400

    Force HTTPS

---
+++ CapSavvInator/App_Code/RequireHttpsPermanentAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace CapSavvInator
+{
+    /// <summary>
+    /// Same as the standard RequireHttps attribute, except the redirect is permanent, 
+    /// and it doesn't throw exceptions for a HEAD request
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    public class RequireHttpsPermanentAttribute : RequireHttpsAttribute
+    {
+        private static String[] _allowedMethods = new[] { "GET", "HEAD" };
+        protected override void HandleNonHttpsRequest(AuthorizationContext filterContext)
+        {
+            if (!_allowedMethods.Contains(filterContext.HttpContext.Request.HttpMethod, StringComparer.OrdinalIgnoreCase))
+            {
+                throw new InvalidOperationException("The requested resource can only be accessed via SSL.");
+            }
+            string url = "https://" + filterContext.HttpContext.Request.Url.Host + filterContext.HttpContext.Request.RawUrl;
+            filterContext.Result = new RedirectResult(url, true);
+        }
+    }
+}
--- CapSavvInator/App_Start/FilterConfig.cs
+++ CapSavvInator/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Diagnostics;
+using System.Web;
 using System.Web.Mvc;
 
 namespace CapSavvInator
@@ -8,6 +9,11 @@ namespace CapSavvInator
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+
+            // Force https (unless we're debugging locally, which doens't support https)
+            if (!Debugger.IsAttached) {
+                filters.Add(new RequireHttpsPermanentAttribute());
+            }
         }
     }
 }
